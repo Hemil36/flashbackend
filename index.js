3e1b1fcfcbc3c4854be7bfc70aff3ad9e0a389cd
DUDHAT HEMIL PRAVINKUMAR <hemildudhat36@gmail.com>
2024-08-12 23:33:21 +0530

    p

--- index.js
+++ index.js
@@ -25,9 +25,9 @@ const port = process.env.PORT || 5000;
 
 
 app.use(credentials);
+app.use(cookieParser());
 app.use(cors(corsOptions));
 app.use(express.json());
-app.use(cookieParser());
 
 
 app.use('/api/users', userRoutes);
